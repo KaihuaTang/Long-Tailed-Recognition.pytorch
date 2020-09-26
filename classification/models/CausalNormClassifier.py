2e289769388331e182a8f10685d41549a1210b08
DESKTOP-PS56VT3\Admin <tkhchipaomian@gmail.com>
2020-09-26 18:32:25 +0800

    no message

--- classification/models/CausalNormClassifier.py
+++ classification/models/CausalNormClassifier.py
@@ -1,17 +1,3 @@
-"""Copyright (c) Facebook, Inc. and its affiliates.
-All rights reserved.
-
-This source code is licensed under the license found in the
-LICENSE file in the root directory of this source tree.
-
-Portions of the source code are from the OLTR project which
-notice below and in LICENSE in the root directory of
-this source tree.
-
-Copyright (c) 2019, Zhongqi Miao
-All rights reserved.
-"""
-
 import torch
 import torch.nn as nn
 from utils import *
